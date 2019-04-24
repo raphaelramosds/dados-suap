7117606a3f90a4dc7bdd9575de5d93f07c9ad0df
Raphael Ramos da Silva <raphael.ramos@academico.ifrn.edu.br>
2019-04-24 09:46:01 -0300

    Subindo

---
+++ js/apisuap.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	
+	$("#botao-login").click(function(e){
+		e.preventDefault();
+		
+		//Aqui são buscados os dados dos campos de login e senha informados pelo usuário no arquivo login.html
+		var username = $("#login").val();
+		var password = $("#senha").val();
+		
+		//Os dados de login e senha são transformados em formato JSON
+		var dadosjson = JSON.stringify({"username": username, "password":password});
+		
+		
+		//Usando AJAX para acessar a API do SUAP
+		$.ajax({
+			//Endereço do serviço para obter a autenticação do usuário
+			url: "https://suap.ifrn.edu.br/api/v2/autenticacao/token/", 
+			dataType: 'json',
+			data: dadosjson,
+			type: 'POST',
+			contentType: 'application/json',
+			success: function (data) { 
+				// alert(JSON.stringify(data)) // Visualizar arquivo retornado em JSON
+				sessionStorage.setItem("token", data.token); //Armazenando o token na seção
+				window.location.href="dados.html"; //Redirecionando para a página dados.html
+			},
+			error: function(data){
+				alert(JSON.stringify(data))// Mostrar JSON dos erros 
+			}
+		});
+
+	});
+
+	$("#botao-sair").click(function(e){
+		sessionStorage.removeItem("token"); // Quebrar seção
+		window.location.href="login.html";
+	});
+
+});
---
+++ js/funcionalidades.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	$("#botao-meusdados").click(function(e){
+		$.ajax({
+				headers: { 
+					"Authorization" : "JWT "+sessionStorage.getItem("token")
+				},
+				url: "https://suap.ifrn.edu.br/api/v2/minhas-informacoes/meus-dados/",
+				contentType: 'application/json',
+				dataType: 'json',
+				type: 'GET',
+				success: function (data) {
+				$('#dados').html(JSON.stringify(data)) // Exibir todos meus dados 
+					$("#usuario-nome_usual").html(data.nome_usual);
+					$("#usuario-tipo_vinculo").html(data.tipo_vinculo);
+					$("#usuario-email").html(data.email);
+					$('#usuario-matricula').html(data.matricula)
+					$('#usuario-img').attr('src', "https://suap.ifrn.edu.br/" + data.url_foto_75x100)
+				},
+				error: function(data){
+					alert("Impossível recuperar dados. Você deve fazer login!");
+					window.location.href = "login.html";
+				}
+		});
+	});
+	
+	
+	/* COLOQUE AS NOVAS FUNCIONALIDADES AQUI ABAIXO, AINDA DENTRO DO $(document).ready */
+		
+});
+
